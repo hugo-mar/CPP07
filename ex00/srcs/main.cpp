aeacb6855d7552cf8ddde7e4954c392505f3aa7f
hugo-mar <hugo.alberto.dias@gmail.com>
2025-08-11 15:39:47 +0100

    Build ex00

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 15:21:39 by hugo-mar          #+#    #+#             */
+/*   Updated: 2025/08/11 15:22:25 by hugo-mar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "someFewFunctions.hpp"
+#include <iostream>
+
+int main(void) {
+    int a = 2;
+    int b = 3;
+
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+    return 0;
+}
---
+++ ex00/srcs/someFewFunctions.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   someFewFunctions.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 15:11:52 by hugo-mar          #+#    #+#             */
+/*   Updated: 2025/08/11 15:28:05 by hugo-mar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOMEFEWFUNCTIONS_HPP
+# define SOMEFEWFUNCTIONS_HPP
+
+template<typename T>
+void swap(T& x, T& y) {
+	T tmp = x;
+	x = y;
+	y = swap;
+}
+
+template<typename T>
+T const& max(T const& x, T const& y) {
+	return ((x > y) ? x : y);
+}
+
+template<typename T>
+T const& min(T const&x, T const& y) {
+	return ((x < y) ? x : y);
+}
+
+#endif
